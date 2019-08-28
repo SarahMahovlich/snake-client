b28d6b262c0a8d293cdc3df627c75fc5d0ead40c
Sarah <sarah.mahov@gmail.com>
2019-08-28 19:39:31 +0000

    updated to include name upon connection

---
+++ client.js
@@ -0,0 +1,29 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function() {
+  const conn = net.createConnection({ 
+    host: '192.168.88.151',
+    port: 50541 
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8'); 
+
+  conn.on('connect', () => {
+    console.log("Successfully connected to game server");
+  });
+  
+  conn.on('connect', () => {
+    conn.write("Name: sjm");
+  });
+
+  conn.on('data', (data) => {
+    console.log('Server says: ', data);
+  });
+
+  return conn;
+}
+
+module.exports = { connect };
